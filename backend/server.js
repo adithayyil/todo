46102a4d9cf1287bedd0abd836e04718577aee0b
adi <1thayyiladi@hdsb.ca>
2023-05-18 22:21:05 -0400

    express setup

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,24 @@
+require('dotenv').config()
 const express = require('express')
+
 
 // initiates express app
 const app = express()
 
+// Middleware
+app.use((request, response, next) => {
+    console.log(request.path, request.method)
+    next()
+})
+
+
+// Routes
+app.get('/', (request, response) => {
+    response.json({msg: 'Welcome to the app'})
+})
+
+
 // listens for requests
-app.listen(4000, () => console.log('listening on port 4000!!'))
+app.listen(4000, () => {
+    console.log('listening on port 4000!!')
+})
